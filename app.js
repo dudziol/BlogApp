cf748ffcd6aedd68e5d0162aa2ca1c912b81e80a
dudzio <dud2iol@gmail.com>
2018-04-29 15:20:21 +0200

    edit route and form addedd

--- app.js
+++ app.js
@@ -61,6 +61,18 @@ app.get("/blogs/:id", function(req, res){
     });
 });
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+             res.render("edit", {blog: foundBlog});
+        }
+    });
+   
+});
+
 app.listen(3000, function(){
     console.log("The server is running!");
 })
